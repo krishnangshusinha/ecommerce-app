6091cc362231a9d470e3fcfde72fd36e52ec61a7
Krishnangshu Sinha <128132191+krishnangshusinha@users.noreply.github.com>
2024-03-25 19:17:51 +0530

    Update useCategory.js

--- client/src/hooks/useCategory.js
+++ client/src/hooks/useCategory.js
@@ -1,13 +1,15 @@
 import { useState , useEffect } from "react";
 import axios from "axios";
 
+let REACT_APP_API = "https://ecommerce-app-sx2y.onrender.com"
+
 export default function useCategory(){
     const [categories , setCategories] = useState([]);
 
     // get all categories
     const getCategories = async () => {
         try{
-          const {data} = await axios.get(`${process.env.REACT_APP_API}/api/v1/category/get-category`); 
+          const {data} = await axios.get(`${REACT_APP_API}/api/v1/category/get-category`); 
           setCategories(data?.category);
 
         }
