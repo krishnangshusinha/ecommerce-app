6b32db1acf47714f92ba81d12a6cea9a2258fc32
Krishnangshu Sinha <128132191+krishnangshusinha@users.noreply.github.com>
2024-03-25 19:28:06 +0530

    Update Products.js

--- client/src/pages/Admin/Products.js
+++ client/src/pages/Admin/Products.js
@@ -5,13 +5,15 @@ import axios from 'axios'
 import toast from 'react-hot-toast'
 import { Link } from 'react-router-dom'
 
+let REACT_APP_API = "https://ecommerce-app-sx2y.onrender.com"
+
 const Products = () => {
     const [products, setProducts] = useState([]);       // list that stores all the products
 
   //getall products
   const getAllProducts = async () => {
     try {
-      const { data } = await axios.get(`${process.env.REACT_APP_API}/api/v1/product/get-product`);   // getting all the products
+      const { data } = await axios.get(`${REACT_APP_API}/api/v1/product/get-product`);   // getting all the products
       setProducts(data.products);// set the products list
     } 
     catch (error) {
@@ -39,7 +41,7 @@ const Products = () => {
                         <Link key={p._id} to={`/dashboard/admin/product/${p.slug}`} className="product-link">
 
                             <div className="card m-2" style={{ width: "18rem" }}>
-                                <img src={`${process.env.REACT_APP_API}/api/v1/product/product-photo/${p._id}`} className="card-img-top" alt={p.name}/>
+                                <img src={`${REACT_APP_API}/api/v1/product/product-photo/${p._id}`} className="card-img-top" alt={p.name}/>
                                 <div className="card-body">
                                     <h5 className="card-title">{p.name}</h5>
                                     <p className="card-text">{p.description}</p>
